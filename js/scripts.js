897921b8d2516fe3737d1858feb49642c91cf422
Szymon Hernik <szymonhernik123@gmail.com>
2020-07-30 00:35:34 +0200

    clean 2

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,6 @@ const vid = document.querySelector("#opening-video");
 const hoverRect = document.querySelector(".hover-space")
 
 
-// Execute this after the main, "heavy" video is loaded
-// This code replaces videos, hides the message
-
 
 
 // DETECT MOBILE FOR MAIN PAGE
@@ -37,64 +34,36 @@ if (vid) {
 
     vid.addEventListener('loadeddata', function() {
       hoverRect.addEventListener('click', () => {
-
-
         if (vid.readyState <= 4) {
           document.body.style.cursor = "wait";
         }
-
       })
-
-
     });
-    // hoverRect.addEventListener('click', () => {
-    //   document.body.style.cursor = "wait";
-    //   vid.addEventListener("canplaythrough", () => {
-    //     document.body.style.cursor = "default";
-    //     el.addEventListener('ended', function() {
-    //
-    //     });
-    //   })
-    // })
-
-    // on click hover rect
-    //   check if loadeddata == true
-    //     // if can play
-    //       when video ended (el.addEventListener('ended', ...);)
-    //         video replace
-    //
-    // (on click: cursor -> wait)
-    //     (when main video can play cursor->pointer)
 
 
 
     vid.addEventListener("loadeddata", () => {
-
 
       let counter = 0;
 
       function counterUpdate() {
         counter++;
       }
+
+      // Execute this after the main, "heavy" video is loaded
+      // This code replaces videos
       el.addEventListener('ended', counterUpdate, false);
-      // let allSources = document.getElementById("face-vid").src;
-
-
-
       //what if the first video ends but you don't click again
 
       hoverRect.addEventListener('click', () => {
+        let waitingTime = 4000;
 
         hoverRect.classList.toggle("hide");
 
-
-        let waitingTime = 4000;
         if (counter == 0) {
-          // document.body.style.cursor ="wait";
           el.addEventListener('ended', myHandler, false);
 
           function myHandler(e) {
-            // document.body.style.cursor ="pointer";
 
             newEl.classList.toggle("hide");
             newEl.classList.toggle("video-styles");
@@ -105,7 +74,6 @@ if (vid) {
           }
         } else if (counter == 1) {
 
-
           newEl.classList.toggle("hide");
           newEl.classList.toggle("video-styles");
           el.parentNode.replaceChild(newEl, el);
@@ -115,27 +83,21 @@ if (vid) {
         }
 
         function showMenu() {
+
           document.querySelector('main').style.visibility = "visible";
           document.querySelector('.background-element').classList.add("opacityIn");
           document.querySelector('nav').classList.add("scaleIn");
-
           run(4000, 5);
-
-
-          console.log(document.getElementById("bg-img").src);
         }
 
       });
-
-
     });
   }
 }
 
+// DEALING WITH LOGO IMAGES CHANGING CONSTANTLY
 let bgImages = document.querySelectorAll("#bg-img")
 let arrayBgImages = Array.from(bgImages)
-
-// console.log(arrayBgImages);
 
 function run(interval, frames) {
   var int = 2;
@@ -150,13 +112,11 @@ function run(interval, frames) {
       int = 1;
     }
   }
-
   var swap = window.setInterval(func, interval);
 }
 
 
-//ABOUT SECTION
-
+// TOP/NAV BAR LOGO OPENING MENU
 if (document.querySelector("section")) {
 
 
@@ -165,7 +125,6 @@ if (document.querySelector("section")) {
     document.querySelectorAll(".element").forEach(element => {
       element.classList.toggle("logoOpen")
     })
-    console.log("clicked");
     document.querySelector(".banner").classList.toggle("active")
     document.querySelector(".navigation-top ul").classList.toggle("active")
   })
@@ -183,125 +142,58 @@ let sectionEvents = document.getElementById("events-section")
 
 
 if (sectionEvents) {
-//   var COL_COUNT = 2; // set this to however many columns you want
-// var col_heights = [],
-//     container = document.getElementById('block-contain');
-// for (var i = 0; i <= COL_COUNT; i++) {
-//   col_heights.push(0);
-// }
-// for (var i = 0; i < container.children.length; i++) {
-//   var order = (i + 1) % COL_COUNT || COL_COUNT;
-//   container.children[i].style.order = order;
-//   col_heights[order] += parseFloat(container.children[i].style.height);
-// }
-// var highest = Math.max.apply(Math, col_heights);
-// container.style.height = highest+'px';
 
   let eventsElements = document.querySelectorAll("ul.events-list")
   let eventsMore = document.querySelectorAll("div.event-more")
   let arrayFromEventsElements = Array.from(eventsElements)
   console.log(arrayFromEventsElements);
-  for (let i = 0; i <arrayFromEventsElements.length; i++) {
-    arrayFromEventsElements[i].addEventListener("click", function (event) {
+  for (let i = 0; i < arrayFromEventsElements.length; i++) {
+    arrayFromEventsElements[i].addEventListener("click", function(event) {
       console.log(i);
-      // if (){
-      //   return;
-      // } else {
-        eventsElements.forEach(element => {
-          element.classList.remove("underline")
-        })
-        eventsElements[i].classList.add("underline")
-        eventsMore.forEach(eventElement => {
-          eventElement.classList.add("hidden")
-        })
-        eventsMore[i].classList.remove("hidden")
-      // }
-
+      eventsElements.forEach(element => {
+        element.classList.remove("underline")
+      })
+      eventsElements[i].classList.add("underline")
+      eventsMore.forEach(eventElement => {
+        eventElement.classList.add("hidden")
+      })
+      eventsMore[i].classList.remove("hidden")
     })
   }
-
-  // arrayFromEventsElements.forEach(function(element) {
-  //   element.addEventListener("click", function (event) {
-  //     console.log(arrayFromEventsElements.indexOf(event.target));
-  //   })
-  // })
-
-
-
-  // arrayFromEventsElements.forEach(eventElement => {
-  //   eventElement.addEventListener("click", function (event) {
-  //
-  //     console.log(eventElement.indexOf(event.target));
-  //
-  //     // if (!eventElement.nextSibling.nextSibling.classList.contains("hidden")) {
-  //     //   return;
-  //     // }
-  //     // else {
-  //     //   eventsElements.forEach(element => {
-  //     //     element.nextSibling.nextSibling.classList.add("hidden")
-  //     //     element.classList.remove("underline")
-  //     //   })
-  //     //   eventElement.classList.add("underline")
-  //     //   eventElement.nextSibling.nextSibling.classList.toggle("hidden")
-  //     // }
-  //
-  //   })
-  // })
-
-  // let last_known_scroll_position = 0;
-  //
-  //
-
-  // let eventsBlocks = document.querySelectorAll(".event-block")
-  // // console.log(eventsElements[0]);
-  //
-  // //put values Y to the variables
-  // // navBar.classList[window.scrollY > whenToSlide ? 'add' : 'remove']('hide')
-  //
-  // let valuesYEventsBlocks = []
-  // // eventsBlocks.forEach(eventBlock => {
-  // //   let yPositionOfBlock = eventBlock.getBoundingClientRect()
-  // //   valuesYEventsBlocks.push(yPositionOfBlock.top)
-  // // })
-  // console.log(valuesYEventsBlocks);
-  // if (last_known_scroll_position==0) {
-  //   eventsElements[0].childNodes[3].classList.add("underline");
-  // }
-  //
-  // //on scroll
-  // //check if eventBlock y value is > window.scrollY
-  //
-  // //check how many elements there is
-  // //
-  // window.addEventListener('scroll', function(e) {
-  //   // last_known_scroll_position = window.scrollY;
-  //   eventsBlocks.forEach(eventBlock => {
-  //
-  //     let yPositionOfBlock = eventBlock.getBoundingClientRect()
-  //     valuesYEventsBlocks.push(yPositionOfBlock.top)
-  //     if (window.scrollY > yPositionOfBlock.top) {
-  //
-  //     }
-  //   })
-  //
-  //   // console.log(last_known_scroll_position);
-  //
-  // });
 }
-
-
-
 
 if (sectionOpenCall) {
 
+  let openCalls = document.querySelectorAll("#block-event")
+  let openCallsMore = document.querySelectorAll(".extra-info")
+  let openCallsMorePoster = document.querySelectorAll("#poster")
+  let arrayFromOpenCallsElements = Array.from(openCalls)
+  
+  for (let i = 0; i < arrayFromOpenCallsElements.length; i++) {
+    arrayFromOpenCallsElements[i].addEventListener("click", function(event) {
+      console.log(i);
+      // openCalls.forEach(element => {
+      //   element.classList.remove("underline")
+      // })
+
+      openCallsMore.forEach(eventElement => {
+        eventElement.classList.add("hidden")
+      })
+      openCallsMorePoster.forEach(eventElement => {
+        eventElement.classList.add("hidden")
+      })
+      openCallsMorePoster[i].classList.remove("hidden")
+      openCallsMore[i].classList.remove("hidden")
+    })
+  }
+
 }
+
 if (sectionArchive || sectionFood) {
   run(7000, 5);
 }
 
 if (sectionAbout) {
-
-  console.log("about");
 
   document.querySelector('.banner').classList.add("banner-animation");
 
@@ -337,6 +229,8 @@ if (sectionAbout) {
   })
 
 }
+
+//SCROLL SMOOTHLY TO THE TARGET ELEMENT
 window.smoothScroll = function(target) {
   var scrollContainer = target;
   do { //find scroll container
@@ -363,6 +257,8 @@ window.smoothScroll = function(target) {
   scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
 
+
+//RECEIVING VALUES FROM VIDEOS FROM ABOUT SECTION AND MATCHING ATTRIBUTES WITH BB NAMES
 let videoNames = document.querySelectorAll("#video-name")
 
 function changeVideo(a) {
@@ -374,7 +270,6 @@ videoNames.forEach(videoName => {
 
   videoName.addEventListener("click", function(e) {
     e.preventDefault()
-    // console.log(e.target);
     videoNames.forEach(videoNameInside => {
       videoNameInside.classList.remove("current")
     })
@@ -383,10 +278,6 @@ videoNames.forEach(videoName => {
     if (videoInView.getAttribute("data-src") === videoName.getAttribute("data-src")) {
       return;
     } else {
-
-      console.log("ss");
-      console.log(videoName.getAttribute("data-src"));
-      console.log(videoInView.getAttribute("data-src"));
       videocontainer.pause();
       videoInView.setAttribute('src', videoName.getAttribute("data-src"));
       videoInView.setAttribute('data-src', videoName.getAttribute("data-src"));
